e153dc17bd5bbbf9434f35581cd5269da4cddbd5
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-10-12 17:06:58 +0200

    adds comments

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
-//cerco di creare i blocchi
+
 const mainContainer = document.getElementById ("main_container");
 
+//creo il ciclo
 for (let i = 1; i <= 100; i++){
+    //creo l'elemento html e gli assegno la classe box (creata da me) e le classi ratio e ratio-1x1 prese da bootstrap
     const scatola = document.createElement ("div");
     scatola.classList.add("box" , "ratio", "ratio-1x1");
     
-    
+    //dichiaro le due variabili che utilizzerò per dare lo stile necessario alle "scatole"
     const treMultiplo = i % 3 === 0;
     const cinqueMultiplo = i % 5 === 0; 
     
-    if(treMultiplo && cinqueMultiplo){
+    
+    if(treMultiplo && cinqueMultiplo){          //questo "if" lo posiziono prima degli "else" così che esclude gli altri due se è vero
         scatola.textContent = "fizzbuzz";
         scatola.classList.add ("bg_tre_cinque_multiplo");
     }else if(treMultiplo){
@@ -18,10 +21,10 @@ for (let i = 1; i <= 100; i++){
     }else if(cinqueMultiplo){
         scatola.textContent = "buzz";
         scatola.classList.add ("bg_cinque_multiplo");
-    }else{
+    }else{                                         
         scatola.textContent = i;
     }
 
 
-    mainContainer.append(scatola)
+    mainContainer.append(scatola)  //mostro la "scatola"
 } 
