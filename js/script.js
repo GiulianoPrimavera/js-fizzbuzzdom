552ece9a0cb22ae37b0a068edc20a8e676c31833
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-10-12 16:06:58 +0200

    adds a cicle "for i" and inside it an "if" with "else if" for multiple of 3 and 5, an "else" for the neutral box, at the end adds the "append" element to visualize the box

--- js/script.js
+++ js/script.js
@@ -4,4 +4,25 @@ const mainContainer = document.getElementById ("main_container");
 for (let i = 1; i <= 100; i++){
     const scatola = document.createElement ("div");
     scatola.classList.add("box");
+    
+    
+    const treMultiplo = i % 3 === 0;
+    const cinqueMultiplo = i % 5 === 0;
+    
+    if(treMultiplo && cinqueMultiplo){
+        scatola.textContent = "fizzbuzz";
+        scatola.classList.add ("bg_tre_cinque_multiplo");
+    }else if(treMultiplo){
+        scatola.textContent = "fizz";
+        scatola.classList.add ("bg_tre_multiplo");
+    }else if(cinqueMultiplo){
+        scatola.textContent = "buzz";
+        scatola.classList.add ("bg_cinque_multiplo");
+    }else{
+        scatola.textContent = i;
+        scatola.classList.add ("bg_neutral");
+    }
+    
+
+    mainContainer.append(scatola)
 } 
